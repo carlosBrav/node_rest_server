5b4f65f9858c6a533b02534325f2c93dc4b7cbef
carlosBrav <carlos.br.carb@gmail.com>
2022-01-16 17:56:45 -0500

    cambios

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,7 @@ const bcrypt = require("bcryptjs");
 
 const getUser = async (req = request, res = response) => {
   const { limite = 5, desde = 0 } = req.query;
-  const query = {  estado: true  };;
-  console.log("GET USER ")
+  const query = { estado: true };
   // const usuarios = await Usuario.find(query)
   //   .skip(Number(desde))
   //   .limit(Number(limite));
